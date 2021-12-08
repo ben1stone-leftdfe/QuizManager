ccdfc846f73a6348d10d823377f7e54d479619de
Ben Stone <ben1.stone@education.gov.uk>
2021-12-08 12:35:44 +0000

    Delete WeatherForecastController

--- src/QuizManager.Api/Startup.cs
+++ src/QuizManager.Api/Startup.cs
@@ -1,20 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using QuizManager.Core.User;
 using QuizManager.Infrastructure.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace QuizManager.Api
 {
